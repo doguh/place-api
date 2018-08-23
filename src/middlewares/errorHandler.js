2b88832ec82b410ef803fd9e5fcfa2b097029e13
hugo <hugo.denisse@gmail.com>
2018-08-23 21:00:07 +0200

    add error handler middleware

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,85 @@
+/**
+ * Express Error handler middleware
+ * @param  {Object}   err  Express error
+ * @param  {Object}   req  Express request
+ * @param  {Object}   res  Express response
+ * @param  {Function} next Express next handler
+ * @returns {void}
+ */
+function errorHandler(err, req, res, next) {
+  if (!err) {
+    return next();
+  }
+
+  const body = {};
+
+  assignJwtError(err, body) ||
+    assignJoiError(err, body) ||
+    assignBadRequestError(err, body) ||
+    assingCommonError(err, body);
+
+  const status = err.status || 500;
+  body.status = status;
+  return res.status(status).json(body);
+}
+
+/**
+ * Build Bad Request error from exception
+ * @param  {Error} err Error object to format
+ * @param  {Object} body Body of the response
+ * @return {Boolean} true if error has been built, false otherwise
+ */
+function assignJwtError(err, body) {
+  if (err.name === "JsonWebTokenError" || err.message === "Missing JWT") {
+    err.status = 401;
+    body.message = err.message;
+    return true;
+  }
+  return false;
+}
+
+/**
+ * Build Bad Request error from exception
+ * @param  {Error} err Error object to format
+ * @param  {Object} body Body of the response
+ * @return {Boolean} true if error has been built, false otherwise
+ */
+function assignBadRequestError(err, body) {
+  if (err.status === 400 && err.details) {
+    err.details.forEach((v, k) => {
+      body[k] = v;
+    });
+    return true;
+  }
+  return false;
+}
+
+/**
+ * Build error from exception
+ * @param  {Error} err Error object to format
+ * @param  {Object} body Body of the response
+ * @return {void}
+ */
+function assingCommonError(err, body) {
+  body.message = err.message;
+  return true;
+}
+
+/**
+ * Build Bad Request error from JOI ValidationError exception
+ * @param  {Error} err Error object to format
+ * @param  {Object} body Body of the response
+ * @return {Boolean} true if error has been built, false otherwise
+ */
+function assignJoiError(err, body) {
+  if (err.name === "ValidationError") {
+    err.status = 400;
+    err.details.forEach(detail => {
+      body[detail.context.key] = [detail.message];
+    });
+    return true;
+  }
+  return false;
+}
+
+module.exports = errorHandler;
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,12 @@
 const bodyParser = require("body-parser");
 const apiRouter = require("./api");
+const errorHandler = require("../middlewares/errorHandler");
 
 module.exports = function configureServer(app) {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
   app.use("/api", apiRouter);
+
+  app.use(errorHandler);
 };
