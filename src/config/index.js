c2d587a0a5e5165edcd46bda124bd9a0c7ce6039
hugo <hugo.denisse@gmail.com>
2018-08-30 13:59:02 +0200

    remove mongo and rabbitmq

--- src/config/index.js
+++ src/config/index.js
@@ -1,14 +1,5 @@
 module.exports = {
   server: {
     port: process.env.PORT || 80
-  },
-  db: {
-    uri: process.env.MONGO_URI || "mongodb://localhost:27017/place",
-    name: "place",
-    collection: "canvas"
-  },
-  amqp: {
-    uri: process.env.AMQP_URI || "amqp://localhost",
-    exchange: "cnv"
   }
 };
--- src/models/canvas.js
+++ src/models/canvas.js
@@ -2,7 +2,6 @@ const mongodb = require("mongodb");
 const amqp = require("amqplib");
 const { Hub } = require("@toverux/expresse");
 const config = require("../config");
-const mongoHelper = require("../helpers/mongo");
 
 /**
  * SSE Hub, used to notify clients of color changes
@@ -10,42 +9,16 @@ const mongoHelper = require("../helpers/mongo");
 const hub = new Hub();
 
 /**
- * Rabbitmq channel used to read/write pixel color change events
- */
-let channel;
-
-/**
- * Initialize the canvas application (database, rabbitmq)
  * @returns {Promise<void>} Promise
  */
-async function init() {
-  // init rabbitmq channel
-  const conn = await amqp.connect(config.amqp.uri);
-  channel = await conn.createChannel();
-  channel.assertExchange(config.amqp.exchange, "fanout", { durable: false });
-  const q = await channel.assertQueue("", { exclusive: true });
-  channel.bindQueue(q.queue, config.amqp.exchange, "");
-  channel.consume(
-    q.queue,
-    function(msg) {
-      const data = JSON.parse(msg.content.toString());
-      hub.data(data);
-    },
-    { noAck: true }
-  );
-  console.log("amqp ready");
-}
+async function init() {}
 
 /**
  * Get the canvas data
  * @returns {Promise<any[]>} the data
  */
 async function getCanvasData() {
-  return mongoHelper
-    .collection(config.db.collection)
-    .find({}, { projection: { _id: 0, x: 1, y: 1, color: 1 } })
-    .sort({ x: 1, y: 1 })
-    .toArray();
+  // TODO
 }
 
 /**
@@ -57,12 +30,7 @@ async function getCanvasData() {
  */
 async function setPixel(x, y, color) {
   // TODO log updates events in another collection
-  await mongoHelper
-    .collection(config.db.collection)
-    .updateOne({ x, y }, { $set: { x, y, color } }, { upsert: true });
-  const msg = JSON.stringify({ x, y, color });
-  channel.publish(config.amqp.exchange, "", Buffer.from(msg));
-  return true;
+  // TODO
 }
 
 module.exports = {
